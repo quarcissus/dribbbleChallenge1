ba23cebaebc06f07de7ccb1567aa70e1569791b9
Gerardo Acosta Olvera <agerardo1397@gmail.com>
2020-07-07 19:45:32 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,40 @@
+TweenMax.to('.landing-page', 2, {
+    delay: .8,
+    x: '0',
+    ease: Power2.easeInOut
+})
+
+// TweenMax.to('.model-photo', 2, {
+//     delay: 2.8,
+//     x: 0,
+//     ease: Power2.easeInOut
+// })
+TweenMax.to('.white-layer', 2, {
+    delay: 2.8,
+    left: "-100%",
+})
+
+TweenMax.to('.white-layer-side', 2, {
+    delay: 4,
+    left: "-100%",
+    ease: Power4.easeOut
+})
+
+TweenMax.to('.white-layer-menu', 1, {
+    delay: 4.7,
+    left: "100%",
+    ease: Power4.easeOut
+})
+
+TweenMax.to('nav', 3, {
+    delay: 4.7,
+    opacity: 1,
+    ease: Power4.easeOut
+})
+
+
+// TweenMax.to('.side-photo', 2, {
+//     delay: 4,
+//     x: 0,
+//     ease: Power2.easeOut
+// })
